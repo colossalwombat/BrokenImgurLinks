b048059c1e13b89ebf93048a51d2d1a57d7ec17a
Jack <jackrichterpowell@gmail.com>
2018-04-02 03:39:44 -0400

    Modularity, recycling and containing to my own subreddit for now

--- bot.py
+++ bot.py
@@ -2,6 +2,39 @@ from praw import *
 import time, arrow, requests
 
 
+
+
+# define the request function
+
+def attempt(url):
+	page = requests.get(post.url)
+
+	#check the html return code
+	#302 is the redirect that for imgur, always indicates the image has been removed
+
+	if page.history and page.history[0].status_code == 302 or page.history[0].status_code == 404:
+		return True
+
+	return False
+
+#define the reply function
+
+def reply(post):
+
+	post.reply(message)
+	print("Got one!")
+
+	#log the reply in the file
+	with open("logfile.txt", "a") as log:
+		log.write("\n")
+		log.write("*******************************\n")
+		log.write("Replied to thread: " + str(post.shortlink) + " at: " + arrow.utcnow().format("YYYY-MM-DD HH:mm:ss") + "\n")
+
+	exit()
+
+	return
+
+
 #init variables
 c_id = secret = user = pwd = message =  ""
 
@@ -14,7 +47,7 @@ with open("keys", 'r') as keys:
 
 reddit = Reddit(client_id=c_id, client_secret=secret, username=user, password=pwd, user_agent='mac:BrokenLinks0.1 (by /u/BrokenImgurLinksBot)')
 
-subreddit = reddit.subreddit('all')
+subreddit = reddit.subreddit('howdoiexist')
 
 with open("logfile.txt", "a") as log:
 	log.write("Successfully logged in at: " + arrow.utcnow().format("YYYY-MM-DD HH:mm:ss") + "\n")
@@ -22,27 +55,35 @@ with open("logfile.txt", "a") as log:
 with open("message.md", "r") as msgfile:
 	message = msgfile.read()
 
-for post in subreddit.stream.submissions():
-	if not "imgur" in post.url:
-		continue
+while True:
 
-	#otherwise check the page for a reddirect
-	page = requests.get(post.url)
+	for post in subreddit.new(limit=100000):
+
+		if arrow.get(post.created_utc) < arrow.utcnow().shift(months=-6):
+			continue
+
+		if not "imgur" in post.url:
+			continue
+
+		if "m.imgur" in post.url:
+			continue
+
+		failures = 0
+
+		while failures < 5:
+			if attempt(post.url):
+				failures += 1
+				time.sleep(5)
+			else:
+				continue
+		
+		#if the loop reaches this point, 5 connection attempts have failed
+		reply(post)
+			
 
 
-	#check the html return code
-	#302 is the redirect that for imgur, always indicates the image has been removed
-	if not page.history: #and page.history[0].status_code == 302:
-		post.reply(message)
-		print("Got one!")
 
-		#log the reply in the file
-		with open("logfile.txt", "a") as log:
-			log.write("\n")
-			log.write("*") * 80
-			log.write("Replied to thread: " + str(post.shortlink) + " at: " + arrow.utcnow().format("YYYY-MM-DD HH:mm:ss"))
-
-		exit()
+		
 
 
 
